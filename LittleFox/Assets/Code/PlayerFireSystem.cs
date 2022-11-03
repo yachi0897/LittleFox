7c1aada53eabf8e985cc72c65ec41a528fc25c7e
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-11-03 16:37:02 +0800

    20221103
    20221103

--- LittleFox/Assets/Code/PlayerFireSystem.cs
+++ LittleFox/Assets/Code/PlayerFireSystem.cs
@@ -8,6 +8,14 @@ namespace FOX
     public class PlayerFireSystem : FireSystemBase
 
     {
-
+        //偵測玩家的輸入行為:鍵盤、滑鼠、觸控、搖桿
+        private void Update()
+        {
+            //如果 玩家 按下 空白鍵 就生成子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
     }
 }
