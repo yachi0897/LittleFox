f79c3f00a6b60a2964ba9575123039cdd04f288f
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-11-03 15:50:08 +0800

    20221104
    20221104

--- LittleFox/Assets/Code/EnemyFireSystem.cs
+++ LittleFox/Assets/Code/EnemyFireSystem.cs
@@ -6,6 +6,15 @@ namespace FOX
     /// </summary>
     public class EnemyFireSystem : FireSystemBase
     {
-        
+        [SerializeField, Header("生成間隔"), Range(0, 3)]
+        private float interval = 1.5f;
+        private void Awake()
+        {
+            //呼叫自訂方法
+            // SpawnBullet();
+            //延遲重複呼叫方法(方法名稱、延遲時間、間隔)
+            InvokeRepeating("SpawnBullet", 0, interval);
+
+        }
     }
 }
--- LittleFox/Assets/Code/FireSystemBase.cs
+++ LittleFox/Assets/Code/FireSystemBase.cs
@@ -12,7 +12,19 @@ namespace   FOX
         private GameObject prefabBullet;
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
-    }
+
+
+        //自訂方法
+        //生成子彈
+        //protected 保護級別 :允許子彈類別存取
+        protected void SpawnBullet()
+        {
+            //實例化(物件、座標、角度)
+            //生成 子彈預置物 角度與座標跟子彈生成點相同
+            Instantiate(prefabBullet, pointSpawn.position, pointSpawn.rotation);
+
+
+        }
 }
 
 
