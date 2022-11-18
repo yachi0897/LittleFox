9a5c58fa15ba480e35b68580f4ba2f3c9493bcd5
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-11-19 06:10:49 +0800

    20221119

---
+++ LittleFox/Assets/Code/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+namespace FOX
+{
+    /// <summary>
+    /// 選單管理器
+    /// </summary>
+    public class MenuManager : MonoBehaviour
+    {
+        //
+        private Button btnPlay;
+
+        private void Awake()
+        {
+            btnPlay = GameObject.Find("開始遊戲").GetComponent<Button>();
+            btnPlay.onClick.AddListener(StartGame);
+        }
+        /// <summary>
+        /// 開始遊戲
+        /// </summary>
+        private void StartGame()
+        {
+            SceneManager.LoadScene("遊戲場景");
+        }
+    }
+}
+
---
+++ LittleFox/Assets/Code/PlayerFinal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+namespace FOX
+{
+    /// <summary>
+    /// 玩家結束管理 :死亡或通關
+    /// </summary>
+    public class PlayerFinal : MonoBehaviour
+    {
+        //被刪除時會執行一次
+        private void OnDestroy()
+        {
+            //死亡
+            Fox.FinalManager.instance.GameOver("挑戰失敗!");
+        }
+        //碰到勾選 Is Trigger 物件時會執行一次
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //過關
+            Fox.FinalManager.instance.GameOver("遊戲通過");
+        }
+    }
+
+}
+
