c4a8f8302c21bcd01f4e42c13dfc5b0e2bbe3a82
yachi0897 <113578359+yachi0897@users.noreply.github.com>
2022-11-18 22:19:40 +0800

    20221119

---
+++ LittleFox/Assets/Code/EnemyFireSystem2.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class EnemyFireSystem2 : MonoBehaviour
+/// <summary>
+/// ¼Ä¤Hµo®g
+/// </summary>
+{
+
+
+
+
+
+ 
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
